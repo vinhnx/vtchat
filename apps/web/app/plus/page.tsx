cfda43a8768a978c4d873575c9428384d7f8530b
Vinh Nguyen <vinhnguyen2308@gmail.com>
2025-05-31 18:22:21 +0700

    fix: remove unnecessary line breaks and adjust styles for improved layout in page.tsx

--- apps/web/app/plus/page.tsx
+++ apps/web/app/plus/page.tsx
@@ -3,14 +3,10 @@ import { PricingTable } from '@clerk/nextjs'
 export default function Page() {
   return (
     <div style={{ maxWidth: '800px', margin: '0 auto', padding: '0 1rem' }}>
-        <br/>
-        <br/>
-        <br/>
-        <br/>
-        <h1 className="scroll-m-20 text-center text-4xl font-extrabold tracking-tight text-balance">
+        <h1 className="scroll-m-20 text-center text-4xl font-extrabold tracking-tight text-balance" style={{ marginTop: '8rem' }}>
             VT has plans that grow with you
         </h1>
-        <p className="text-muted-foreground text-xl">
+        <p className="text-muted-foreground text-xl" style={{ textAlign: 'center', marginBottom: '2rem' }}>
             Explore how AI can help you with everyday tasks. Level up productivity and creativity with expanded access.
         </p>
         <PricingTable />
