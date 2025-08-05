880b9f3ac6aa306d2a7df556d55408fb19d5dda4
Vinh Nguyen <vinhnguyen2308@gmail.com>
2025-08-05 20:44:19 +0700

    Update about

--- apps/web/app/about/page.tsx
+++ apps/web/app/about/page.tsx
@@ -76,7 +76,7 @@ function AboutContent() {
                     <iframe
                         width="560"
                         height="315"
-                        src="https://www.youtube-nocookie.com/embed/5ChJ-b3Ie-k?si=HTu07olzi2RA_ZSm&amp;controls=0"
+                        src="https://www.youtube-nocookie.com/embed/5ChJ-b3Ie-k?si=RphgJ7OMT67Xmcpc&amp;controls=0"
                         title="YouTube video player"
                         frameBorder="0"
                         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
